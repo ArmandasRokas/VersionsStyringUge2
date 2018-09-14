80a375a6670918f892b3752114b268c1cc386b90
Armandas <armandas.rokas@gmail.com>
2018-09-14 15:27:17 +0200

    Initial commit

---
+++ src/Kinematics.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Kinematics {
+
+    public static void main(String[] args) {
+
+        double altitude; // meters
+        double time;  // seconds
+        double finalSpeed;  // m/s
+        double accelerationSpeed = 9.82; // m/s*s
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Enter altitude: ");
+        altitude = s.nextDouble();
+
+        time = Math.sqrt(2*altitude/accelerationSpeed);
+        finalSpeed = accelerationSpeed*time;
+
+        System.out.println("It is going to take: " + time + " seconds");
+        System.out.println("And the final speed is: " + finalSpeed + " m/s");
+        s.close();
+    }
+}
+
